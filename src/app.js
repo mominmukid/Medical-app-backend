eb861e9a341986ac0281e50d997cb9dc0878066a
Mukid Momin <mominmukid@gmail.com>
2025-11-15 16:02:52 +0530

    feat: initialize medical backend project with essential configurations and utilities
    - Added package.json with dependencies for the backend application.
    - Created app.js to set up Express server with middleware for CORS, JSON parsing, and cookie parsing.
    - Introduced constants.js for standard database name.
    - Implemented database connection logic in db/index.js using Mongoose.
    - Set up server entry point in server.js to start the application and connect to the database.
    - Developed ApiError and ApiResponce utility classes for standardized error and response handling.
    - Added asyncHandler utility for handling asynchronous route handlers.
    - Integrated Cloudinary for image upload and deletion functionalities in cloudinary.js and deleteImageCloudinary.js.
    - Configured Google OAuth2 client in googleClient.js for future authentication needs.

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+// this is the standard middleware for CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://wideview.netlify.app",
+    ],
+    credentials: true,
+  })
+);
+
+// this is the standard middleware for parsing JSON requests
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("Hello this is the Backend of Explorer APP");
+});
+//routes  import
+// import userRouter from "./routes/user.routes.js";
+
+
+//routes decleration
+// app.use("/api/v1/users", userRouter)
+
+
+export default app;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+
+// this is the standard database name for the application
+ const DB_NAME = "Mediacal AppDB";
+export {
+   DB_NAME
+}
